494b4ef4a3bad01c5c6800cbd43d4abd49a91474
Thomasprelot <thompas.prelot@gmail.com>
2020-11-17 16:35:16 +0100

    event list and api calls set

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    >
+    </event-card>
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.',
+        })
+      })
+  },
   head() {
     return {
       title: 'Event Listing',
