d6514ea98813aa4ac4e2affdcf8e1f7caae74926
Thomasprelot <thompas.prelot@gmail.com>
2020-11-17 16:34:52 +0100

    Changed axios port

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: { baseURL: 'http://localhost:3000' },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
